068b2a32c24ef8fd7e0c17931b6fe7684b4700a3
Tiago Cruz <tcruz@netic.io>
2023-10-16 22:21:54 +0200

    Melhoras e Adicionados Fotos de Jogadores e Logos das equipas

--- details_matches.js
+++ details_matches.js
@@ -4,27 +4,41 @@ const fs = require("fs").promises;
 const path = require("path");
 const { BASE_URL } = require("./config");
 
+// Función para descargar una imagen desde una URL y guardarla en una ruta especificada
 const downloadImage = async (imageUrl, imagePath) => {
     try {
+        // Fetch the image data as an array buffer
         const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+        // Create the directory for the image if it doesn't exist
         await fs.mkdir(path.dirname(imagePath), { recursive: true });
+        // Write the image data to the specified path
         await fs.writeFile(imagePath, response.data);
-        console.log(`Image downloaded: ${imagePath}`);
     } catch (error) {
         console.error(`Error downloading image: ${imageUrl}`, error);
     }
 };
+
+// Función para guardar detalles de archivos JSON en una carpeta especificada
 const saveDetails = async (folderRoot) => {
+    const playersFolderPath = path.join(folderRoot, "players");
+    try {
+        // Check if the players folder already exists
+        await fs.access(playersFolderPath);
+    } catch (error) {
+        // Create the players folder if it doesn't exist
+        await fs.mkdir(playersFolderPath, { recursive: true });
+    }
     const matchesFolderPath = path.join(__dirname, "matches");
     try {
+        // Read the files in the matches folder
         const files = await fs.readdir(matchesFolderPath, { withFileTypes: true });
-
         for (const file of files) {
             const filePath = path.join(matchesFolderPath, file.name);
-
             if (file.isFile() && file.name.endsWith(".json")) {
-                await processDetails(filePath, folderRoot, matchesFolderPath);
+                // Process details for JSON files
+                await processDetails(filePath, folderRoot);
             } else if (file.isDirectory()) {
+                // Process subdirectories recursively
                 await processSubdirectory(filePath, folderRoot, matchesFolderPath);
             }
         }
@@ -33,17 +47,17 @@ const saveDetails = async (folderRoot) => {
     }
 };
 
+// Función para procesar un subdirectorio dentro de la carpeta de partidos
 const processSubdirectory = async (directoryPath, folderRoot, matchesFolderPath) => {
     try {
+        // Read the files in the subdirectory
         const subFiles = await fs.readdir(directoryPath, { withFileTypes: true });
-
         for (const subFile of subFiles) {
             const subFilePath = path.join(directoryPath, subFile.name);
-
             if (subFile.isFile() && subFile.name.endsWith(".json")) {
-                const matchId = subFile.name.split(".")[0]; // Obtener el ID del partido del nombre del archivo
-                const matchFolderPath = path.join(matchesFolderPath, matchId); // Ruta de la carpeta del partido
-
+                // Process details for JSON files within the subdirectory
+                const matchId = subFile.name.split(".")[0];
+                const matchFolderPath = path.join(matchesFolderPath, matchId);
                 await processDetails(subFilePath, folderRoot, matchFolderPath);
             }
         }
@@ -52,97 +66,106 @@ const processSubdirectory = async (directoryPath, folderRoot, matchesFolderPath)
     }
 };
 
-const processDetails = async (filePath) => {
+// Función para procesar los detalles de un archivo JSON
+const processDetails = async (filePath, folderRoot, matchesFolderPath) => {
     try {
+        // Read the JSON file
         const fileData = await fs.readFile(filePath, "utf8");
         const jsonData = JSON.parse(fileData);
-
         if (Array.isArray(jsonData)) {
             for (const item of jsonData) {
                 if (item.gameLink) {
                     try {
+                        // Fetch the HTML data from the game link
                         const response = await axios.get(item.gameLink);
                         const html = response.data;
                         const root = parse(html);
                         const gameResume = root.querySelector(".game-resume");
-                        const infoTimePlace = root.querySelector(".info-time-place");
                         const infoGoals = root.querySelector(".info-goals");
-
-                        if (!gameResume || !infoTimePlace || !infoGoals) {
-                            console.error(`Error fetching data from ${item.gameLink}: Required elements not found`);
+                        const refereePrincipal = root.querySelector(".lineup-team");
+                        if (!gameResume || !infoGoals || !refereePrincipal) {
+                            // Skip if required elements are not found
                             continue;
                         }
-
-                        const infoTimePlaceText = infoTimePlace.text.trim();
+                        // Extract relevant data from the HTML
                         const homeTeamLogo = gameResume.querySelector(".col-md-1 img");
                         const homeTeamNameElement = gameResume.querySelector(".col-md-3");
                         const scoreElement = gameResume.querySelector(".col-md-4 strong");
                         const awayTeamNameElement = gameResume.querySelector(".col-md-3.text-right");
                         const awayTeamLogo = gameResume.querySelector(".col-md-1.text-right img");
-
-                        const homeTeamName = homeTeamNameElement.text.trim();
-                        const score = scoreElement.text.trim();
-                        const awayTeamName = awayTeamNameElement.text.trim();
-
-                        const homeTeamInfoGoals = infoGoals.querySelector(".col-md-3.text-left")?.text || "";
-                        const awayTeamInfoGoals = infoGoals.querySelector(".col-md-3.text-right")?.text || "";
-
-                        const matchFolderPath = path.dirname(filePath); // Ruta de la carpeta del partido
-                        const detailsFolderPath = matchFolderPath;
+                        const playerPhotos = refereePrincipal.querySelectorAll(".photo img");
+                        const playerNumbers = refereePrincipal.querySelectorAll("strong");
+                        const playerNames = refereePrincipal.querySelectorAll("strong + span");
+                        const homeTeamName = homeTeamNameElement.textContent.trim();
+                        const score = scoreElement.textContent.trim();
+                        const awayTeamName = awayTeamNameElement.textContent.trim();
+                        const homeTeamInfoGoals = infoGoals.querySelector(".col-md-3.text-left")?.textContent || "";
+                        const awayTeamInfoGoals = infoGoals.querySelector(".col-md-3.text-right")?.textContent || "";
+                        if (!playerPhotos || !playerNumbers || !playerNames) {
+                            // Skip if player data is not found
+                            continue;
+                        }
+                        const detailsFolderPath = matchesFolderPath;
                         await fs.mkdir(detailsFolderPath, { recursive: true });
-
                         const matchId = item.gameLink.match(/matchId=(\d+)/)[1];
                         const fileName = `${matchId}.json`;
                         const detailsFilePath = path.join(detailsFolderPath, fileName);
-
                         const gameData = {
                             homeTeamLogo: homeTeamLogo ? homeTeamLogo.getAttribute("src") : null,
                             homeTeamName,
                             score,
                             awayTeamName,
                             awayTeamLogo: awayTeamLogo ? awayTeamLogo.getAttribute("src") : null,
-                            infoTimePlace: infoTimePlaceText,
                             homeTeamInfoGoals,
                             awayTeamInfoGoals,
+                            players: []
                         };
-
                         let homeTeamLogoUrl = homeTeamLogo ? homeTeamLogo.getAttribute("src") : null;
                         let awayTeamLogoUrl = awayTeamLogo ? awayTeamLogo.getAttribute("src") : null;
-
-                        const homeTeamLogoPath = homeTeamLogoUrl
-                            ? path.join(__dirname, "teams_logos", `${path.basename(homeTeamLogoUrl)}.png`)
-                            : null;
-                        const awayTeamLogoPath = awayTeamLogoUrl
-                            ? path.join(__dirname, "teams_logos", `${path.basename(awayTeamLogoUrl)}.png`)
-                            : null;
-
-                        if (homeTeamLogoPath && !homeTeamLogoUrl.startsWith("http") && !homeTeamLogoUrl.startsWith("https")) {
-                            homeTeamLogoUrl = `${BASE_URL}${homeTeamLogoUrl}`;
+                        const players = [];
+                        for (let i = 0; i < playerPhotos.length; i++) {
+                            const playerPhoto = playerPhotos[i];
+                            const playerNumber = playerNumbers[i].textContent.trim();
+                            const playerName = (playerNames[i]?.textContent || '').trim();
+                            if (playerPhoto && playerNumber && playerName) {
+                                const playerData = {
+                                    playerPhoto: playerPhoto.getAttribute("src"),
+                                    playerNumber,
+                                    playerName
+                                };
+                                players.push(playerData);
+                            }
                         }
-
-                        if (awayTeamLogoPath && !awayTeamLogoUrl.startsWith("http") && !awayTeamLogoUrl.startsWith("https")) {
-                            awayTeamLogoUrl = `${BASE_URL}${awayTeamLogoUrl}`;
+                        gameData.players = players;
+                        let homeTeamLogoPath = null;
+                        let awayTeamLogoPath = null;
+                        if (homeTeamLogoUrl) {
+                            homeTeamLogoPath = path.join(__dirname, "teams_logos", `${path.basename(homeTeamLogoUrl)}.png`);
+                            if (!homeTeamLogoUrl.startsWith("http") && !homeTeamLogoUrl.startsWith("https")) {
+                                homeTeamLogoUrl = `${BASE_URL}${homeTeamLogoUrl}`;
+                            }
                         }
-
+                        if (awayTeamLogoUrl) {
+                            awayTeamLogoPath = path.join(__dirname, "teams_logos", `${path.basename(awayTeamLogoUrl)}.png`);
+                            if (!awayTeamLogoUrl.startsWith("http") && !awayTeamLogoUrl.startsWith("https")) {
+                                awayTeamLogoUrl = `${BASE_URL}${awayTeamLogoUrl}`;
+                            }
+                        }
+                        const promises = [];
                         if (homeTeamLogoPath) {
-                            await downloadImage(homeTeamLogoUrl, homeTeamLogoPath);
+                            promises.push(downloadImage(homeTeamLogoUrl, homeTeamLogoPath));
                         }
-
                         if (awayTeamLogoPath) {
-                            await downloadImage(awayTeamLogoUrl, awayTeamLogoPath);
+                            promises.push(downloadImage(awayTeamLogoUrl, awayTeamLogoPath));
                         }
-
-                        // Guardar el objeto gameData en el archivo JSON
+                        await Promise.all(promises);
+                        // Write the game data to a JSON file
                         await fs.writeFile(
                             detailsFilePath,
                             JSON.stringify(gameData, null, 2)
                         );
-                        console.log(`Game resume saved to ${detailsFilePath}`);
                     } catch (error) {
-                        console.error(
-                            `Error while fetching data from ${item.gameLink}:`,
-                            error
-                        );
+                        console.error(`Error while fetching data from ${item.gameLink}:`, error);
                     }
                 }
             }
