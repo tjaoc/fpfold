03c2f6bffe88dcde6cb6feaabeb1376d4b6486a7
Tiago Cruz <tcruz@netic.io>
2023-10-26 01:51:32 +0200

    Up and Team Logo Download

--- details_matches.js
+++ details_matches.js
@@ -4,7 +4,7 @@ const fs = require("fs").promises;
 const path = require("path");
 const { BASE_URL } = require("./config");
 
-const downloadImage = async (imageUrl, imagePath) => {
+const downloadPlayerPhoto = async (imageUrl, imagePath) => {
     try {
         const response = await axios.get(BASE_URL + imageUrl, { responseType: "arraybuffer" });
         const playersFolderPath = path.join(__dirname, "players");
@@ -14,6 +14,19 @@ const downloadImage = async (imageUrl, imagePath) => {
         await fs.writeFile(newImagePath, response.data);
     } catch (error) {
         console.error(`Error downloading image: ${imageUrl}`, error);
+    }
+};
+
+const downloadTeamLogo = async (imageUrl, imagePath) => {
+    try {
+        const response = await axios.get(BASE_URL + imageUrl, { responseType: "arraybuffer" });
+        const teamsLogoFolderPath = path.join(__dirname, "teams_logos");
+        await fs.mkdir(teamsLogoFolderPath, { recursive: true });
+        const imageName = path.basename(imagePath);
+        const newImagePath = path.join(teamsLogoFolderPath, `${imageName}.png`);
+        await fs.writeFile(newImagePath, response.data);
+    } catch (error) {
+        console.error(`Error downloading team logo: ${imageUrl}`, error);
     }
 };
 
@@ -69,7 +82,7 @@ const processDetails = async (filePath, folderRoot, matchFolderPath, matchId) =>
                     try {
                         // Add a delay of 1 second before making the request
                         await new Promise(resolve => setTimeout(resolve, 1000));
-                        
+
                         const response = await axios.get(item.gameLink);
                         const html = response.data;
                         const root = parse(html);
@@ -85,8 +98,26 @@ const processDetails = async (filePath, folderRoot, matchFolderPath, matchId) =>
                                 name
                             });
                         });
+                        const teamLogoData = [];
+                        const teams = root.querySelectorAll(".section-title.game-resume");
+                        teams.forEach((team) => {
+                            const teamLogo = team.querySelector("img").getAttribute("src");
+                            teamLogoData.push({
+                                teamLogo
+                            });
+                        });
 
-                        if (playerData.length > 0) {
+                        const teamsLogoFolderPath = path.join(folderRoot, "teams_logos");
+                        await fs.mkdir(teamsLogoFolderPath, { recursive: true });
+                        for (const team of teamLogoData) {
+                            if (team.teamLogo) {
+                                const imageName = path.basename(team.teamLogo);
+                                const newImagePath = path.join(teamsLogoFolderPath, `${imageName}`);
+                                await downloadTeamLogo(team.teamLogo, newImagePath);
+                            }
+                        }
+
+                        if (playerData.length > 0 && teamLogoData.length > 0) {
                             const name = item.gameLink.split("matchId=")[1].split("&")[0];
                             const detailsFilePath = path.join(matchFolderPath, `${name}.json`);
                             await fs.writeFile(detailsFilePath, JSON.stringify(playerData, null, 2));
@@ -97,7 +128,7 @@ const processDetails = async (filePath, folderRoot, matchFolderPath, matchId) =>
                             for (const player of playerData) {
                                 const imageName = path.basename(player.photo);
                                 const newImagePath = path.join(playersFolderPath, `${imageName}`);
-                                await downloadImage(player.photo, newImagePath);
+                                await downloadPlayerPhoto(player.photo, newImagePath);
                             }
                         }
                     } catch (error) {
